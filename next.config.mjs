3484ed7f1f51f8586719bd0cc273e70ef18f5799
Ibaliqbal <iqbalmuthahhary@gmail.com>
2024-07-24 18:35:49 +0700

    reset: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "phtftpjuazktjjpqmkwu.supabase.co",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ui-avatars.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
     domains: [
       "lh3.googleusercontent.com",
       "phtftpjuazktjjpqmkwu.supabase.co",
