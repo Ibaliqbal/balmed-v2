cdc162553a2c94025a71891f57afd4959b0aa6f9
Ibaliqbal <iqbalmuthahhary@gmail.com>
2024-07-24 18:19:54 +0700

    update: again

--- src/app/( lobby )/( main )/[user]/status/[id]/page.tsx
+++ src/app/( lobby )/( main )/[user]/status/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = ({ params }: { params: { user: string; id: string } }) => {
   return (
     <div>
-      Hello {params.user} {params.id}
+      Hello {params.user} {params.id} guys
     </div>
   );
 };
